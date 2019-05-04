c353852810c9231ad75459c05807fa8847d23326
Paula Engelberg <engelberg.paula@gmail.com>
2019-05-04 11:19:53 +0200

    init

---
+++ docs/app.js
@@ -0,0 +1,12 @@
+document.querySelectorAll('select').forEach(element => {
+
+    element.innerHTML = `
+
+        <option value="EUR">EUR</option>
+        <option value="USD">USD</option>
+        <option value="CHF">CHF</option>
+        <option value="SEK">SEK</option>
+    `;
+
+});
+
